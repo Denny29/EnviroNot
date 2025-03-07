f00122110cab030f7951eee4899625f4aac80acb
Denny Fernandez <dennyrfernandez2004@gmail.com>
2025-03-07 15:21:21 -0500

    Added some more to the fracking page, going to commit now to add bootstrap

--- src/pages/Fracking.js
+++ src/pages/Fracking.js
@@ -1,7 +1,11 @@
 const Fracking = () => {
     return (
         <div>
-            <h1>We love fracking</h1>
+            <div className="header">
+                <h1>We love fracking</h1>
+                <h2>And you should too.</h2>
+            </div>
+            <p>Here at [insert evil name], fracking isnt just our job, it's our way of life</p>
         </div>
     )
 }

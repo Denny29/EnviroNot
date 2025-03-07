29641a3153447a24071f499a2cf31f1bba97a292
Denny Fernandez <dennyrfernandez2004@gmail.com>
2025-03-07 16:22:59 -0500

    Updated routing for the new pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Fracking from "./pages/Fracking";
+import Initiatives from "./pages/Initiatives";
+import Leaderboard from "./pages/Leaderboard";
+import Petition from "./pages/Petition";
+import Merch from "./pages/Merch";
 import NoPage from "./pages/NoPage";
 
 export default function App() {
@@ -13,6 +17,10 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="Fracking" element={<Fracking />} />
+          <Route path="Initiatives" element={<Initiatives />} />
+          <Route path="Leaderboard" element={<Leaderboard />} />
+          <Route path="Petition" element={<Petition />} />
+          <Route path="Merch" element={<Merch />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>

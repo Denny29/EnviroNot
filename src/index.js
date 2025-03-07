17cf9199c0c48aa3ab8bb4f87affa22b765a02e1
Denny Fernandez <dennyrfernandez2004@gmail.com>
2025-03-07 15:11:01 -0500

    Added a Fracking page and updated the routing stuff for it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
+import Fracking from "./pages/Fracking";
 import NoPage from "./pages/NoPage";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="Fracking" element={<Fracking />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
---
+++ src/pages/Fracking.js
@@ -0,0 +1,9 @@
+const Fracking = () => {
+    return (
+        <div>
+            <h1>We love fracking</h1>
+        </div>
+    )
+}
+
+export default Fracking;

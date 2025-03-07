e94d13d751b294406a433ae3e4881ddbcd98e6ed
Keith McDade <kp.mac09@gmail.com>
2025-03-07 16:25:42 -0500

    formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,23 @@
 
 const Home = () => {
     return (
-      <div>
-      <h1>Home</h1>
-      <p> Are you tired of fresh air, clean water, and trees? <br></br>
+      <div class="d-flex flex-column ">
+        <div class="d-flex justify-content-center text-center p-2 mb-2 bg-secondary text-white">
+          Are you tired of fresh air, clean water, and trees? <br></br>
           Does the sight of healthy fish and wildlife make you sick? <br></br>
           Do you long for a world where you can swim in toxic sludge and fill your lungs with smog? <br></br>
           If you said yes to any of these questions, enviroNOT is here to help you achieve your dreams! <br></br> <br></br>
+        </div>
+        <div class="d-flex justify-content-center text-center p-2">
           At enviroNOT we’ve compiled every tool available to DESTROY the environment in one convenient place! <br></br>
           Want to create more smog? Want to dump some toxic waste? Want to clear cut an entire forest? <br></br>
-          <b>We’ve got you covered!</b> </p>
+        </div>
+        <div class="d-flex justify-content-center text-center p-2">
+          <b>We’ve got you covered!</b> 
+        </div>
+        <div class="d-flex justify-content-center p-2" >
+          
+        </div>
       </div>
   );
   };

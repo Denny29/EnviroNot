b313404723dc86c7996163f864b2a14b05dbcbe0
Denny Fernandez <dennyrfernandez2004@gmail.com>
2025-03-07 15:11:01 -0500

    Added a Fracking page and updated the routing stuff for it

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -8,6 +8,9 @@ const Layout = () => {
             <li>
               <Link to="/">Home</Link>
             </li>
+            <li>
+              <Link to="/Fracking">Fracking</Link>
+            </li>
           </ul>
         </nav>
   

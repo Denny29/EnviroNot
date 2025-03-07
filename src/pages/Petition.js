efc4a03a811e12d57b30d1d94c44fcaf73280411
Denny Fernandez <dennyrfernandez2004@gmail.com>
2025-03-07 17:51:14 -0500

    Changed a sentence in petition.js

--- src/pages/Petition.js
+++ src/pages/Petition.js
@@ -2,7 +2,7 @@ const Petition = () => {
     return (
         <div style={{marginTop: "50px", marginLeft:"10px"}}>
             <h1 style={{textAlign: "center"}}>Do YOU agree with our mission?</h1>
-            <p>Then show solidarity to our cause by signing this Petition</p>
+            <p>Then show solidarity to our cause by signing this petition!</p>
 
             <form>
                 <label>Name </label> <br></br>
